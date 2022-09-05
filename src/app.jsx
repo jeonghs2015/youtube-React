73ccc91c616f95c29e51ef6cb2686950214b87fb
hsjung2015 <hsjung201558@gmail.com>
2022-09-05 16:44:12 +0900

    postmas- javascript fetch 라이브러리 사용

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [videos, setVideos] = useState([]);
+  const [name, setName] = useState('jeonghs');
+
+  useEffect(()=>{
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAgRTpkgwgjkowpxmrlyk1p0dYeNuiprsY", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  }, []);
   return (
    <h1>Hello :)</h1>
   );
