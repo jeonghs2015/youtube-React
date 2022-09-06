0cc2cf97cef6f67184d81d33ec702edba5656839
hsjung2015 <hsjung201558@gmail.com>
2022-09-06 11:04:11 +0900

    list, item css 수정 및 마크업 작업

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,7 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
+import styles from './video_item.module.css';
 
-const VideoItem = (props) => <li>
-    <img src={props.video.snippet.thumbnails.medium.url} alt="video thumbnail" />
-</li>;
+const VideoItem = ({ video: { snippet } }) => (
+  <li className={styles.container}>
+    <div className={styles.video}>
+      <img
+        className={styles.thumbnail}
+        src={snippet.thumbnails.medium.url}
+        alt="video thumbnail"
+      />
+      <div className={styles.metadata}>
+        <p className={styles.title}>{snippet.title}</p>
+        <p className={styles.channel}>{snippet.channelTitle}</p>
+      </div>
+    </div>
+  </li>
+);
 
 export default VideoItem;
